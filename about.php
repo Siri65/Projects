343f9211ec285c3a0272b8c83bf6a7156ccd0e35
Mohana Siri Vasamsetty <21A95A0506@aec.edu.in>
2023-06-16 20:12:53 +0530

    Add files via upload
    Committed!

---
+++ about.php
@@ -0,0 +1,141 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>about</title>
+
+   <!-- swiper css link  -->
+   <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+   
+<!-- header section starts  -->
+
+<section class="header">
+
+   <a href="home.php" class="logo">Travel.</a>
+
+   <nav class="navbar">
+      <a href="home.php">Home</a>
+      <a href="about.php">About</a>
+      <a href="package.php">Package</a>
+      <a href="book.php">Book</a>
+   </nav>
+
+   <div id="menu-btn" class="fas fa-bars"></div>
+
+</section>
+
+<!-- header section ends -->
+
+<div class="heading" style="background:url(images/header-bg-1.png) no-repeat">
+   <h1>About Us</h1>
+</div>
+
+<!-- about section starts  -->
+
+<section class="about">
+
+   <div class="image">
+      <img src="images/about-img.jpeg" alt="reload">
+   </div>
+
+   <div class="content">
+      <h3>Why Choose Us?</h3>
+      <p>Our search system helps you to find a personlized tour in just few clicks,you'll find a perfect destination among the hundreds available.
+         Our high level servcie is offically recognized by the thousands of clients by providing the 24/7 support.</p>
+      <p>Provide hassel free travel at affordable prices and offers great cuisines and experience unique cultures.</p>
+      <div class="icons-container">
+         <div class="icons">
+            <i class="fas fa-map"></i>
+            <span>Top Destinations</span>
+         </div>
+         <div class="icons">
+            <i class="fas fa-hand-holding-usd"></i>
+            <span>Affordable Price</span>
+         </div>
+         <div class="icons">
+            <i class="fas fa-headset"></i>
+            <span>24/7 Guide Service</span>
+         </div>
+      </div>
+   </div>
+
+</section>
+
+<!-- about section ends -->
+
+
+
+
+<!-- footer section starts  -->
+
+<section class="footer">
+
+   <div class="box-container">
+
+      <div class="box">
+         <h3>quick links</h3>
+         <a href="home.php"> <i class="fas fa-angle-right"></i> Home</a>
+         <a href="about.php"> <i class="fas fa-angle-right"></i> About</a>
+         <a href="package.php"> <i class="fas fa-angle-right"></i> Package</a>
+         <a href="book.php"> <i class="fas fa-angle-right"></i> Book</a>
+      </div>
+
+      <div class="box">
+         <h3>extra links</h3>
+         <a href="#"> <i class="fas fa-angle-right"></i> Ask Questions</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> About Us</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> Privacy Policy</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> Terms of Use</a>
+      </div>
+
+      <div class="box">
+         <h3>contact info</h3>
+         <a href="#"> <i class="fas fa-phone"></i> +123-456-7890 </a>
+         <a href="#"> <i class="fas fa-phone"></i> +111-444-7777 </a>
+         <a href="#"> <i class="fas fa-envelope"></i> mohanasiri0011@gmail.com </a>
+         <a href="#"> <i class="fas fa-map"></i> mumbai, india - 400104 </a>
+      </div>
+
+      <div class="box">
+         <h3>follow us</h3>
+         <a href="#"> <i class="fab fa-facebook-f"></i> facebook </a>
+         <a href="#"> <i class="fab fa-twitter"></i> twitter </a>
+         <a href="#"> <i class="fab fa-instagram"></i> instagram </a>
+         <a href="#"> <i class="fab fa-linkedin"></i> linkedin </a>
+      </div>
+
+   </div>
+
+   <div class="credit"> created by <span>Mohana Siri Vasamsetty</span> | all rights reserved! </div>
+
+</section>
+
+<!-- footer section ends -->
+
+
+
+
+
+
+
+
+
+<!-- swiper js link  -->
+<script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
+
+<!-- custom js file link  -->
+<script src="js/script.js"></script>
+
+</body>
+</html>
---
+++ package.php
@@ -0,0 +1,278 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>package</title>
+
+   <!-- swiper css link  -->
+   <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+   
+<!-- header section starts  -->
+
+<section class="header">
+
+   <a href="home.php" class="logo">travel.</a>
+
+   <nav class="navbar">
+      <a href="home.php">Home</a>
+      <a href="about.php">About</a>
+      <a href="package.php">Package</a>
+      <a href="book.php">Book</a>
+   </nav>
+
+   <div id="menu-btn" class="fas fa-bars"></div>
+
+</section>
+
+<!-- header section ends -->
+
+<div class="heading" style="background:url(images/header-bg-2.png) no-repeat">
+   <h1>packages</h1>
+</div>
+
+<!-- packages section starts  -->
+
+<section class="packages">
+
+   <h1 class="heading-title">top destinations</h1>
+
+   <div class="box-container">
+
+      <div class="box">
+         <div class="image">
+            <img src="images/img1.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>Adventure Trip</h3>
+            <p>Find adventure trips, tours, outdoor activities and adventure holiday packages. 
+               Join our Tribes, learn from Gurus and find great adventures.</p>
+            <a href="book.php" class="btn">Book Now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/img2.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>Historical</h3>
+            <p>Exploring these historical sites are the best in the world. 
+               Get inspiration for your next trip as well as practical tips on how to visit each spot!</p>
+            <a href="book.php" class="btn">Book Now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/img3.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>Beach & Resort</h3>
+            <p>Great hotels & resorts for your perfect vacation, the ideal getaway with family or friends.
+               Explore world wide beaches and islands,with your family.</p>
+            <a href="book.php" class="btn">Book Now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/img4.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>Waterfalls</h3>
+            <p>Visiting these waterfalls brings the opportunity to view some of the most spectacular sights on earth.
+               Get Travel Deals in Your Inbox.</p>
+            <a href="book.php" class="btn">Book Now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/img5.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>Romantic Tour</h3>
+            <p>Whether you're organizing a far-flung Romantic Destinations or a closer-to-home minibreak, 
+               planning the trip of a lifetime for two can feel overwhelming.</p>
+            <a href="book.php" class="btn">Book Now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/img6.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>Concerts</h3>
+            <p>Customized performing tours abroad - build your own, choose a classic tour, or plan an international 
+               festival or parade experience.</p>
+            <a href="book.php" class="btn">Book Now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/img7.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>Water Parks</h3>
+            <p>The Coolest Water Parks Around the World. Grab your tube and check out the best slides, 
+               rides and pools for a whole day of family fun.</p>
+            <a href="book.php" class="btn">Book Now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/img8.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>Picnic Spot</h3>
+            <p>Family holiday packages - Browse through a wide range of Family packages with exciting offers. 
+               Book customized family tour packages with exciting deals & offers.</p>
+            <a href="book.php" class="btn">Book Now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/img9.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>Museums</h3>
+            <p>Book & customize your holiday package for Museum - Best Museum tour package provides the enjoyment of tourist</p>
+            <a href="book.php" class="btn">Book Now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/img10.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>Art Gallery</h3>
+            <p>Find perfect tours and trips to enjoy your Art vacations or holidays. Explorer photo, review and more. Book Now.</p>
+            <a href="book.php" class="btn">Book Now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/img11.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>Fashion Show</h3>
+            <p>All Travel Packages include local resort accommodations for people plus VIP or GA passes 
+               to the entire Fashion Weeks for your best holiday trip!</p>
+            <a href="book.php" class="btn">Book Now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/img12.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>Treeking</h3>
+            <p>Let's get going and discover the great outdoors through these treks in worldwide.
+               Explorer photo, review and more. Book Now.</p>
+            <a href="book.php" class="btn">Book Now</a>
+         </div>
+      </div>
+
+   </div>
+
+   <div class="load-more"><span class="btn">Load More...</span></div>
+
+</section>
+
+<!-- packages section ends -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- footer section starts  -->
+
+<section class="footer">
+
+   <div class="box-container">
+
+      <div class="box">
+         <h3>quick links</h3>
+         <a href="home.php"> <i class="fas fa-angle-right"></i> Home</a>
+         <a href="about.php"> <i class="fas fa-angle-right"></i> About</a>
+         <a href="package.php"> <i class="fas fa-angle-right"></i> Package</a>
+         <a href="book.php"> <i class="fas fa-angle-right"></i> Book</a>
+      </div>
+
+      <div class="box">
+         <h3>extra links</h3>
+         <a href="#"> <i class="fas fa-angle-right"></i> Ask Questions</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> About Us</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> Privacy Policy</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> Terms of Use</a>
+      </div>
+
+      <div class="box">
+         <h3>contact info</h3>
+         <a href="#"> <i class="fas fa-phone"></i> +123-456-7890 </a>
+         <a href="#"> <i class="fas fa-phone"></i> +111-444-7777 </a>
+         <a href="#"> <i class="fas fa-envelope"></i> mohanasiri0011@gmail.com </a>
+         <a href="#"> <i class="fas fa-map"></i> Kakinada,India - 533002 </a>
+      </div>
+
+      <div class="box">
+         <h3>follow us</h3>
+         <a href="#"> <i class="fab fa-facebook-f"></i> facebook </a>
+         <a href="#"> <i class="fab fa-twitter"></i> twitter </a>
+         <a href="#"> <i class="fab fa-instagram"></i> instagram </a>
+         <a href="#"> <i class="fab fa-linkedin"></i> linkedin </a>
+      </div>
+
+   </div>
+
+   <div class="credit"> created by <span>Mohana Siri Vasamsetty</span> | all rights reserved! </div>
+
+</section>
+
+<!-- footer section ends -->
+
+
+
+
+
+
+
+
+
+<!-- swiper js link  -->
+<script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
+
+<!-- custom js file link  -->
+<script src="js/script.js"></script>
+
+</body>
+</html>
